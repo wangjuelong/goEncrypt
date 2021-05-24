937674e6b5c2ae3749c86d0cc7e9a840f89edc7d
lamba <lamba.wang@rtshield.com>
2021-05-24 23:55:22 +0800

    新增rsa-example

--- example/rsa/main.go
+++ example/rsa/main.go
@@ -1,3 +1,20 @@
-package rsa
- 
-$END$
+package main
+
+import (
+	"fmt"
+	"github.com/wangjuelong/goEncrypt"
+)
+
+func main() {
+	pri, pub, err := goEncrypt.GetRsaKey()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("private key:")
+	fmt.Println(pri.String())
+
+	fmt.Println("public key:")
+	fmt.Print(pub.String())
+}
